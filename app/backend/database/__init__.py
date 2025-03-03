d575ddf2ecaf5e91e893d682aa88a582cf173885
tamlong5 <dexter.tam@mail.utoronto.ca>
2025-03-02 22:30:42 -0500

    Add login/signup abstract methods to DAO interface

--- app/backend/database/__init__.py
+++ app/backend/database/__init__.py
@@ -1,35 +1,194 @@
 from abc import ABC, abstractmethod
-
+from datetime import datetime
+from typing import Optional
 
 class DataAccessObject(ABC):
     """A database access object to interface with user data."""
-
+    
     @abstractmethod
-    def add_user(self, username: str, email: str, password: str) -> None:
-        """Add a new user into the database with the given username, email, 
-        and hashed password.
+    def user_exists(self, username: str, email: str) -> bool:
+        """Return whether the username or email already exists in the database.
 
         Args:
-            username: The username of the new user.
-            email: The email of the new user.
-            password: The hashed password of the new user.
+            username: The username to check.
+            email: The email to check.
         
         Raises:
             DatabaseError: An error related to the database occurred.
             DataError: An error related to the processed data occurred.
         """
         raise NotImplementedError
-    
+
     @abstractmethod
-    def user_exists(self, username: str) -> bool:
-        """Return whether the username already exists in the database.
+    def add_user(self, username: str, email: str, password: str,
+                 auth_provider: str) -> None:
+        """Insert a new user in the database with the given information.
 
         Args:
-            username: The username to check.
+            username: The username of the user.
+            email: The email of the user.
+            password: The hashed password of the user.
+            auth_provider: The authentication provider used to create the 
+                           account.
+
+        Raises:
+            DatabaseError: An error related to the database occurred.
+            DataError: An error related to the processed data occurred.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_user_by_id(self, id: int
+        ) -> Optional[
+        tuple[str, str, str, str, datetime, datetime, Optional[datetime]]]:
+        """Fetch the user information for the user associated with the given id.
+        
+        Args:
+            id: The id of the user.
+        
+        Returns:
+            None if no user exists with the given id. Otherwise, a tuple 
+            (username, email, password, auth_provider, created_at, updated_at,
+            last_login):
+             
+            username (str): The username of the user.
+            email (str): The email of the user.
+            password (str): The hashed password of the user.
+            auth_provider (str): The authentication provider that the 
+                                    account is associated with, either local 
+                                    or Google. The value is either "local" 
+                                    or "google".
+            created_at (datetime): The time at which the account was 
+                                    created.
+            updated_at (datetime): The last time at which the account was 
+                                    updated.
+            last_login (datetime): The last time the user logged into this 
+                                    account.
+
+        Raises:
+            DatabaseError: An error related to the database occurred.
+            DataError: An error related to the processed data occurred.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_user_by_email(self, email: str
+        ) -> Optional[
+        tuple[str, str, str, str, datetime, datetime, Optional[datetime]]]:
+        """Fetch the user information for the user associated with the given 
+        email.
+        
+        Args:
+            email: The email of the user.
+        
+        Returns:
+            None if no user exists with the given email. Otherwise, a tuple 
+            (username, email, password, auth_provider, created_at, updated_at,
+            last_login):
+
+            username (str): The username of the user.
+            email (str): The email of the user.
+            password (str): The hashed password of the user.
+            auth_provider (str): The authentication provider that the 
+                                    account is associated with, either local 
+                                    or Google. The value is either "local" 
+                                    or "google".
+            created_at (datetime): The time at which the account was 
+                                    created.
+            updated_at (datetime): The last time at which the account was 
+                                    updated.
+            last_login (datetime): The last time the user logged into this 
+                                    account.
+
+        Raises:
+            DatabaseError: An error related to the database occurred.
+            DataError: An error related to the processed data occurred.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_user_by_username(self, username: str
+        ) -> Optional[
+        tuple[str, str, str, str, datetime, datetime, Optional[datetime]]]:
+        """Fetch the user information for the user associated with the given 
+        username.
+        
+        Args:
+            username: The username of the user.
+        
+        Returns:
+            None if no user exists with the given username. Otherwise, a tuple 
+            (username, email, password, auth_provider, created_at, 
+            updated_at, last_login):
+             
+            username (str): The username of the user.
+            email (str): The email of the user.
+            password (str): The hashed password of the user.
+            auth_provider (str): The authentication provider that the 
+                                    account is associated with, either local 
+                                    or Google. The value is either "local" 
+                                    or "google".
+            created_at (datetime): The time at which the account was 
+                                    created.
+            updated_at (datetime): The last time at which the account was 
+                                    updated.
+            last_login (datetime): The last time the user logged into this 
+                                    account.
+
+        Raises:
+            DatabaseError: An error related to the database occurred.
+            DataError: An error related to the processed data occurred.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_refresh_token(self, token: str, revoked: bool
+    ) -> Optional[tuple[int, datetime, datetime]]:
+        """Return the information associated with the given token and revoked 
+        status.
+
+        Args:
+            token: The refresh token.
+            revoked: The revoked status of the token.
+        
+        Returns:
+            None if the token could not be found. Otherwise, a tuple
+            (user_id, expires_at, created_at):
+
+            user_id: The user associated with this token.
+            expires_at: The time at which the token expires.
+            created_at: The time at which the token was created.
         
         Raises:
             DatabaseError: An error related to the database occurred.
             DataError: An error related to the processed data occurred.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_token_revoked(self, token: str, revoked: bool) -> None:
+        """Update the revoked status of the given token to the given value.
+        
+        Args:
+            token: The refresh token.
+            revoked: The new revoked status of the token.
+
+        Raises:
+            DatabaseError: An error related to the database occurred.
+            DataError: An error related to the processed data occurred.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_last_login(self, username: str, time: datetime) -> bool:
+        """Update the last login time of the user to the given time.
+        
+        Args:
+            username: The username of the user.
+            time: The time to which the last login time is updated.
+
+        Raises:
+            DatabaseError: An error related to the database occurred.
         """
         raise NotImplementedError
     
@@ -97,4 +256,3 @@ class DataAccessObject(ABC):
             DataError: An error related to the processed data occurred.
         """
         raise NotImplementedError
-    
