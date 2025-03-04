1c0ec28ed14d74da5ecd60815f64657f2807aca5
tamlong5 <dexter.tam@mail.utoronto.ca>
2025-03-03 20:02:18 -0500

    Add basic SQLite implementation of DAO

--- app/backend/database/sqlitedb.py
+++ app/backend/database/sqlitedb.py
@@ -1,86 +1,146 @@
-from database import DataAccessObject
 import sqlite3
 import datetime
 
-SCHEMA = "database/schema.ddl"
+from typing import Optional
+
+from backend.database import DataAccessObject, AuthProvider, SortOrder, Filter
+
+
+SCHEMA = "backend/database/schema.ddl"
 
 class SQLiteDB(DataAccessObject):
     """An SQLite implementation of the data access object."""
-    filename: str
+    conn: sqlite3.Connection
     
-    def __init__(self, filename: str = "database/database.db"):
+    def __init__(self, filename: str = "backend/database/sqlite.db"):
         """Initialise the database with the given filename."""
-        self.filename = filename
-
         with open(SCHEMA, "r") as file:
             ddl_script = file.read()
-        with sqlite3.connect(self.filename) as conn:
-            cur = conn.cursor()
-            cur.executescript(ddl_script)
-            conn.commit()
 
-    def add_user(self, username, email, password):
-        with sqlite3.connect(self.filename) as conn:
-            cur = conn.cursor()
-            cur.execute("INSERT INTO user (id, username, email, password) VALUES (NULL, ?, ?, ?)",
-                        (username, email, password))
-            conn.commit()
+        self.conn = sqlite3.connect(filename)
+        cur = self.conn.cursor()
+        cur.executescript(ddl_script)  # read the schema into the db
+        self.conn.commit()
 
-    def user_exists(self, username):
-        with sqlite3.connect(self.filename) as conn:
-            cur = conn.cursor()
-            cur.execute("SELECT id FROM user WHERE user.username = ?", (username))
-            return cur.fetchone()
-    
-    def create_exam(self, username, name, color, description, public):
-        with sqlite3.connect(self.filename) as conn:
-            cur = conn.cursor()
+    def user_exists(self, 
+        username: Optional[str], 
+        email: Optional[str]
+    ) -> bool:
+        cur = self.conn.cursor()
 
-            # get the user id
-            cur.execute("SELECT id FROM user WHERE username = ?", username)
-            user_id = cur.fetchone()
-
-            cur.execute("INSERT INTO exam (examId, name, date, owner, color, description, public) VALUES (NULL, ?, ?, ?, ?, ?, ?)", 
-                        (name, datetime.datetime.now().isoformat(), user_id, color, description, public))
-            conn.commit()
-
-    def insert_question(self, username, exam, question, answers):
-        with sqlite3.connect(self.filename) as conn:
-            cur = conn.cursor()
-
-            # get the user id
-            cur.execute("SELECT id FROM user WHERE username = ?", (username,))
-            user_id = cur.fetchone()[0]
-            # get the exam id
-            cur.execute("SELECT examID FROM exam WHERE owner = ? AND name = ?",
-                        (user_id, exam))
-            exam_id = cur.fetchone()[0]
-            cur.execute("INSERT INTO question (questionId, number, exam, question) VALUES (NULL, ?, ?, ?)",
-                        (..., exam_id, question))
+        if username:
+            cur.execute("SELECT * FROM User WHERE username = ?;", (username,))
+        elif email:
+            cur.execute("SELECT * FROM User WHERE email = ?;", (email,))
+        else:
+            return None
+        
+        return cur.fetchone is not None
             
-            # inswert answers
-            question_id = cur.lastrowid
-            cur.executemany("INSERT INTO answers (answer, question, answer, confidence) VALUES (NULL, ?, ?, ?)", 
-                            [(question_id, answer_text, confidence) for answer_text, confidence in answers])
-            conn.commit()
+    def add_user(self, 
+        username: str, 
+        email: str, 
+        password: str,
+        auth_provider: AuthProvider, 
+        oauth_id: Optional[str] = None
+    ) -> None:
+        cur = self.conn.cursor()
+        time = datetime.now()
+        
+        cur.execute("INSERT INTO User "
+                    "(username, email, password, auth_provider, oauth_id) "
+                    "VALUES "
+                    "(?, ?, ?, ?, ?);",
+                    (username, email, password, auth_provider, oauth_id))
+        self.conn.commit()
 
-    def get_exams(self, username, public):
-        with sqlite3.connect(self.filename) as conn:
-            cur = conn.cursor()
+    def get_user_by_id(self, 
+        id: int
+    ) -> Optional[tuple[str, str, str, AuthProvider, 
+                        datetime, datetime, Optional[datetime]]]:
+        cur = self.conn.cursor()
 
-            # get the user id
-            cur.execute("SELECT id FROM user WHERE username = ?", (username,))
-            user_id = cur.fetchone()[0]
+        cur.execute("SELECT * FROM User "
+                    "WHERE id = ?;",
+                    (id,))
 
-            if public:
-                cur.execute("SELECT examId FROM exam WHERE owner = ? AND public = 1", (user_id,))
-            else:
-                cur.execute("SELECT examId FROM exam WHERE owner = ?", (user_id,))
-            exam_ids = cur.fetchall()
+        return cur.fetchone()
+            
+    def get_user_by_email(self, 
+        email: str
+    ) -> Optional[tuple[str, str, str, AuthProvider, 
+                        datetime, datetime, Optional[datetime]]]:
+        cur = self.conn.cursor()
 
-        return exam_ids
+        cur.execute("SELECT * FROM User "
+                    "WHERE email = ?;",
+                    (email,))
 
+        return cur.fetchone()
 
+    def get_user_by_username(self, 
+        username: str
+    ) -> Optional[tuple[str, str, str, AuthProvider, 
+                        datetime, datetime, Optional[datetime]]]:
+        cur = self.conn.cursor()
+
+        cur.execute("SELECT * FROM User "
+                    "WHERE username = ?;",
+                    (username,))
+
+        return cur.fetchone()
+
+    def get_refresh_token(self, 
+        token: str, 
+        revoked: bool
+    ) -> Optional[tuple[int, datetime, datetime]]:
+        cur = self.conn.cursor()
+
+        cur.execute("SELECT user, expires_at, created_at FROM RefreshToken "
+                    "WHERE token = ? AND revoked = ?;",
+                    (token, revoked))
+
+        return cur.fetchone()
+    
+    def set_token_revoked(self, token: str, revoked: bool) -> None:
+        cur = self.conn.cursor()
+
+        cur.execute("UPDATE RefreshToken "
+                    "SET revoked = ? "
+                    "WHERE token = ?;")
+
+    def update_last_login(self, username: str, time: datetime) -> bool:
+        cur = self.conn.cursor()
+
+        cur.execute("UPDATE User"
+                    "SET last_login = ?"
+                    "WHERE username = ?")
+
+    def get_exams(self, 
+        user_id: str,
+        sorting: SortOrder,
+        filter: Filter,
+        title: Optional[str]
+    ) -> list[tuple[int, str, str, str, str]]:
+        raise NotImplementedError
+
+    def create_exam(self,
+        username: str,
+        name: str,
+        color: str,
+        description: str,
+        public: bool
+    ) -> None:
+        raise NotImplementedError
+
+    def insert_question(self,
+        username: str,
+        exam: str,
+        question: str,
+        answers: set[tuple[str, float]]
+    ) -> None:
+        raise NotImplementedError
+    
 
 if __name__ == "__main__":
     db = SQLiteDB()
