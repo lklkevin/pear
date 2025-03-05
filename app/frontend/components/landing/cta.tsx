33038b1193f1b6491086cde4f8b1b3c441fe35bd
likevi53 <lkevin.li@mail.utoronto.ca>
2025-03-05 04:48:19 -0500

    fix: ui changes

--- app/frontend/components/landing/cta.tsx
+++ app/frontend/components/landing/cta.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const CTA = () => {
   return (
-    <section className="text-center fixed bottom-0 mb-12 sm:mb-0 left-0 right-0 sm:static md:mt-24 z-10">
+    <section className="text-center fixed bottom-0 mb-12 sm:mb-0 left-0 right-0 sm:static md:mt-24 z-10 ">
       <Link href="/generate" passHref>
-        <button className="z-10 group mx-auto w-[20rem] sm:w-[26rem] max-w-lg flex items-center justify-between rounded-2xl sm:rounded-3xl bg-emerald-200 bg-opacity-15 text-white py-3 sm:py-4 px-6 border border-white font-medium text-lg text-left transition-colors duration-200 hover:bg-opacity-25">
-          <span>Try now for free</span>
+        <button className="drop-shadow-xl backdrop-blur-lg z-10 group mx-auto w-[20rem] sm:w-[26rem] max-w-lg flex items-center justify-between rounded-2xl sm:rounded-3xl bg-emerald-300 bg-opacity-15 text-white py-3 sm:py-4 px-6 border border-white font-medium text-lg text-left transition-colors duration-200 hover:bg-opacity-20">
+          <span className="drop-shadow-xl">Try now for free</span>
           <span className="material-icons text-3xl transition-transform duration-200 group-hover:translate-x-2">
             arrow_forward
           </span>
