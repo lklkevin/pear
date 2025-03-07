2ec1b7611f7e3128d25c148bf4a9a3bb72b8bd74
likevi53 <lkevin.li@mail.utoronto.ca>
2025-03-06 21:31:16 -0500

    feat: finished connected generate and save endpoints

--- app/frontend/components/generate/examForm.tsx
+++ app/frontend/components/generate/examForm.tsx
@@ -7,6 +7,15 @@ import { useSession, getSession } from "next-auth/react";
 import Counter from "./counter";
 import { useErrorStore, useLoadingStore } from "@/store/store";
 import { useRouter } from "next/router";
+import {
+  VisibilityOption,
+  Visibility,
+} from "@/components/form/visibilityOption";
+import {
+  StylingOptions,
+  Color,
+  colors,
+} from "@/components/form/stylingOptions";
 
 export default function ExamForm() {
   const router = useRouter();
@@ -15,6 +24,8 @@ export default function ExamForm() {
   const [examTitle, setExamTitle] = useState("");
   const [examDescription, setExamDescription] = useState("");
   const { data: session } = useSession();
+  const [visibility, setVisibility] = useState<Visibility>("private");
+  const [selectedColor, setSelectedColor] = useState<Color>("teal");
 
   useEffect(() => {
     if (!localStorage.getItem("browserSessionId")) {
@@ -23,24 +34,19 @@ export default function ExamForm() {
   }, []);
 
   const handleGenerate = async () => {
-    // If there's no active session (i.e., guest user)
-    if (!session) {
-      // Clear any previous errors in the global state
-      useErrorStore.getState().setError(null);
+    const formData = new FormData();
+    files.forEach((file) => formData.append("files", file));
+    formData.append("title", examTitle || "Untitled Exam");
+    formData.append(
+      "description",
+      examDescription || "No description was provided"
+    );
+    formData.append("num_questions", count.toString());
+    useErrorStore.getState().setError(null);
 
-      // Prepare the FormData with all required fields and files
-      const formData = new FormData();
-      files.forEach((file) => formData.append("files", file));
-      formData.append("title", examTitle || "Untitled Exam");
-      formData.append(
-        "description",
-        examDescription || "No description was provided"
-      );
-      formData.append("num_questions", count.toString());
-      console.log(formData);
-
+    // If there's no active session (i.e., guest user) or unsaved exam
+    if (!session || visibility === "unsaved") {
       try {
-        // Make the POST request to the backend endpoint
         useLoadingStore.getState().setLoading(true);
         const response = await fetch(
           `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/exam/generate`,
@@ -96,7 +102,6 @@ export default function ExamForm() {
           useLoadingStore.getState().setLoading(false);
         }
       } catch (error) {
-        // Handle errors by setting the global error state
         if (error instanceof Error) {
           useErrorStore
             .getState()
@@ -107,19 +112,62 @@ export default function ExamForm() {
         useLoadingStore.getState().setLoading(false);
       }
     } else {
-      // Optionally handle the case for authenticated users
-      console.log("Authenticated user detected. Handle accordingly if needed.");
+      const privacyValue = visibility === "public" ? "1" : "0";
+      formData.append("privacy", privacyValue);
+
+      const selectedColorHex =
+        colors.find((c) => c.value === selectedColor)?.hex || "#3f3f46";
+      formData.append("color", selectedColorHex);
+
+      const currSession = await getSession();
+
+      try {
+        useLoadingStore.getState().setLoading(true);
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/exam/generate/save`,
+          {
+            headers: {
+              Authorization: `Bearer ${currSession?.accessToken}`,
+            },
+            method: "POST",
+            body: formData,
+          }
+        );
+
+        const result = await response.json();
+        if (!response.ok) {
+          useErrorStore.getState().setError("Error generating exam");
+          useLoadingStore.getState().setLoading(false);
+          return;
+        }
+
+        if (result.message) {
+          useErrorStore.getState().setError(result.message);
+        } else {
+          console.log(result);
+        }
+
+        useLoadingStore.getState().setLoading(false);
+      } catch (error) {
+        if (error instanceof Error) {
+          useErrorStore
+            .getState()
+            .setError(error.message || "Error generating exam");
+        } else {
+          useErrorStore.getState().setError("Error generating exam");
+        }
+        useLoadingStore.getState().setLoading(false);
+      }
     }
   };
 
   return (
-    <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-8  h-full">
+    <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-8 h-full">
       {/* Left: File Upload */}
       <div className="flex flex-col h-full">
         <h3 className="text-lg sm:text-xl font-semibold mb-2 sm:mb-4">
           File Upload
         </h3>
-        {/* The container below will fill the remaining space */}
         <div className="flex-1">
           <FileUpload files={files} setFiles={setFiles} />
         </div>
@@ -157,6 +205,44 @@ export default function ExamForm() {
               step={1}
             />
           </div>
+
+          {session && (
+            <>
+              <h3 className="text-lg sm:text-xl font-semibold mt-4 sm:mt-8 mb-2 sm:mb-4">
+                Visibility
+              </h3>
+              <div className="flex-col sm:flex-row flex gap-4 w-full">
+                <VisibilityOption
+                  option="private"
+                  selected={visibility === "private"}
+                  label="Private"
+                  subText="Only visible to you"
+                  onChange={() => setVisibility("private")}
+                />
+                <VisibilityOption
+                  option="public"
+                  selected={visibility === "public"}
+                  label="Public"
+                  subText="Visible to everyone"
+                  onChange={() => setVisibility("public")}
+                />
+                <VisibilityOption
+                  option="unsaved"
+                  selected={visibility === "unsaved"}
+                  label="Unsaved"
+                  subText="Will not be saved"
+                  onChange={() => setVisibility("unsaved")}
+                />
+              </div>
+            </>
+          )}
+
+          {session && visibility !== "unsaved" && (
+            <StylingOptions
+              selectedColor={selectedColor}
+              setSelectedColor={setSelectedColor}
+            />
+          )}
         </div>
         {/* mt-auto pushes the button container to the bottom */}
         <div className="text-lg mt-auto">
